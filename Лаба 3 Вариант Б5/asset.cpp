b659abdb3794ba2485c0a83bdeab2ec0c89e77f9
staskond <staskond@gmail.com>
2015-12-20 01:05:12 +0200

    исправлен мелкий недочет

--- Лаба 3 Вариант Б5/asset.cpp
+++ Лаба 3 Вариант Б5/asset.cpp
@@ -12,7 +12,7 @@
 
 
 
-Asset::Asset(const std::string & _FullNameProperty, int _cost)
+Asset::Asset(const std::string & _FullNameProperty, double _cost)
 	:m_FullNameProperty(_FullNameProperty),
 	m_cost(_cost)
 {

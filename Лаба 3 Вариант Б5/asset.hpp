03e30b6fec8ad6a0bebd1bcb1727ea794240ef41
staskond <staskond@gmail.com>
2015-12-19 20:27:51 +0200

    часть сделана

--- Лаба 3 Вариант Б5/asset.hpp
+++ Лаба 3 Вариант Б5/asset.hpp
@@ -2,19 +2,29 @@
 
 #ifndef _ASSET_HPP_
 #define _ASSET_HPP_
-
+#include <string>
+#include "messages.hpp"
 /*****************************************************************************/
 
 
 class Asset
 {
+public:
+	Asset(const std::string & _FullNameProperty, int _cost);
+	virtual ~Asset() = default;
 
-/*-----------------------------------------------------------------*/
+	virtual int GetSumCostOfProperty() { return 0; };
+	virtual int CrashProperty() { return 0; };
+	virtual int RepairingProperty() { return 0; };
 
-	// TODO
+	const std::string & _FullNameProperty() const { return m_FullNameProperty; };
+	int GetCost() const { return m_cost; };
 
-/*-----------------------------------------------------------------*/
+private:
+	const std::string & m_FullNameProperty;
+	int m_cost;
 
+	
 };
 
 

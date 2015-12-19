9831881a8660607e6ae349b85506aad4f1700857
staskond <staskond@gmail.com>
2015-12-19 20:29:56 +0200

    вапвпвап

--- Лаба 3 Вариант Б5/asset.cpp
+++ Лаба 3 Вариант Б5/asset.cpp
@@ -9,3 +9,11 @@
 
 
 /*****************************************************************************/
+
+Asset::Asset(const std::string & _FullNameProperty, int _cost)
+	:m_FullNameProperty(_FullNameProperty),
+	m_cost(_cost)
+{
+	if (m_cost < 0)
+		throw std::logic_error(Messages::NegativeAssetBaseCost);
+}
